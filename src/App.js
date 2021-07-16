4e837f3f8e3250c8d63f8a00a00f97aba0ced003
ross-u <cirkovic.uros.srb@gmail.com>
2021-07-16 10:18:47 +0200

    Example 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
+import Counter from "./components/01-use-state/Counter";
 
 function App() {
-  return <div className="App"></div>;
+  const [show, setShow] = useState(true);
+
+  return (
+    <div className="App">
+      <button onClick={() => setShow(!show)}>
+        { show ? "Hide" : "Show"}
+      </button>
+      {show && <Counter />}
+    </div>
+  );
 }
 
 export default App;
