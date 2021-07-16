e940dfd204372e19f18cc31ab36a35fc9c8d860f
ross-u <cirkovic.uros.srb@gmail.com>
2021-07-16 19:12:53 +0200

    Example 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import "./App.css";
 import Counter from "./components/01-use-state/Counter";
+import Timer from "./components/02-use-state-loop/Timer";
+
+import TimerTwo from "./components/03-use-effect-mounting/TimerTwo";
 
 function App() {
   const [show, setShow] = useState(true);
@@ -10,7 +13,10 @@ function App() {
       <button onClick={() => setShow(!show)}>
         { show ? "Hide" : "Show"}
       </button>
-      {show && <Counter />}
+      {/* {show && <Counter />} */}
+      {/* {show && <Timer />} */}
+
+      {show && <TimerTwo />}
     </div>
   );
 }
