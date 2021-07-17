e17ac0d80381c0b1b3815534b15691b5ca24f7d5
ross-u <cirkovic.uros.srb@gmail.com>
2021-07-17 14:31:12 +0200

    Example 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import Counter from "./components/01-use-state/Counter";
+import Counter from "./components/01-use-state/Counter";    // <== IMPORT
 
 function App() {
   const [show, setShow] = useState(true);
@@ -10,7 +10,7 @@ function App() {
       <button onClick={() => setShow(!show)}>
         { show ? "Hide" : "Show"}
       </button>
-      {show && <Counter />}
+      {show && <Counter />}                       {/*  <== ADD  */}
     </div>
   );
 }
