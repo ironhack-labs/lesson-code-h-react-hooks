f10e7af6a9d43bd55abdce8f827cb8c87a7aea35
ross-u <cirkovic.uros.srb@gmail.com>
2021-07-17 22:27:23 +0200

    Example 6 - fetching data

---
+++ src/components/IronbnbList.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import Spinner from "./Spinner";
+import axios from "axios";
+
+const apiURL = "https://ironbnb-m3.herokuapp.com/apartments";
+
+function IronbnbList() {
+  const [fetching, setFetching] = useState(true);
+  const [apartments, setApartments] = useState([]);
+
+  useEffect(() => {
+    console.log("useEffect - Initial render (Mounting)");
+    axios.get(apiURL).then((response) => {
+      setApartments(response.data);
+      setFetching(false);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h3>List of apartments</h3>
+      {fetching && <Spinner />}
+
+      {apartments.map((apt) => (
+        <div key={apt._id} className="card">
+          <img src={apt.img} alt="apartment" />
+          <h3>{apt.title}</h3>
+          <p>Price: {apt.pricePerDay}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default IronbnbList;
